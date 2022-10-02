582d6b291671d101fa72e4761b2a34aa6b28efc1
chyngyz <chyngyzbek452@gmail.com>
2022-10-02 12:00:37 +0600

    read.md

---
+++ second.py
@@ -0,0 +1,13 @@
+n = 'chiko'
+s = 'zupu'
+a = '25'
+g = 'm'
+w = 'amazon'
+c = 'light code'
+print('name:', n)
+print('surname:', s)
+print('age:', a)
+print('gender:', g)
+print('work:', w)
+print('course:', c)
+ 
---
+++ task2.py
@@ -0,0 +1,15 @@
+device_name = 'chyngyz lenovo v 15'
+Hardware_model = 'lenovo v 15'
+memory = '8,0gib'
+processor = 'adm ryzen 35300'
+graphic = 'amd renior'
+disk_capacity = '256,1gb'
+OS_name = 'ubuntu 22.04'
+print ('device_name:', device_name)
+print ('Hardware_model:', Hardware_model)
+print ('memory:', memory)
+print ('processor:', processor)
+print ('graphic:', graphic)
+print ('disk_capacity:', disk_capacity)
+print ('OS_name:', OS_name)
+
